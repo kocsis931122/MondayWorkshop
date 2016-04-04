5a8a9e05afcc16c3e02d46c93b59869b976e00b2
Kocsis Viktor <achilleus88888@gmail.com>
2016-04-04 15:48:58 +0200

    Finish
    finish

---
+++ MondayWorkshop/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace MondayWorkshop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelAddress = new System.Windows.Forms.Label();
+            this.labelAge = new System.Windows.Forms.Label();
+            this.labelMobile = new System.Windows.Forms.Label();
+            this.labelType = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxAddress = new System.Windows.Forms.TextBox();
+            this.textBoxAge = new System.Windows.Forms.TextBox();
+            this.textBoxMobile = new System.Windows.Forms.TextBox();
+            this.comboBoxType = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(43, 73);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(35, 13);
+            this.labelName.TabIndex = 0;
+            this.labelName.Text = "Name";
+            // 
+            // labelAddress
+            // 
+            this.labelAddress.AutoSize = true;
+            this.labelAddress.Location = new System.Drawing.Point(43, 108);
+            this.labelAddress.Name = "labelAddress";
+            this.labelAddress.Size = new System.Drawing.Size(45, 13);
+            this.labelAddress.TabIndex = 1;
+            this.labelAddress.Text = "Address";
+            // 
+            // labelAge
+            // 
+            this.labelAge.AutoSize = true;
+            this.labelAge.Location = new System.Drawing.Point(43, 135);
+            this.labelAge.Name = "labelAge";
+            this.labelAge.Size = new System.Drawing.Size(26, 13);
+            this.labelAge.TabIndex = 2;
+            this.labelAge.Text = "Age";
+            // 
+            // labelMobile
+            // 
+            this.labelMobile.AutoSize = true;
+            this.labelMobile.Location = new System.Drawing.Point(43, 172);
+            this.labelMobile.Name = "labelMobile";
+            this.labelMobile.Size = new System.Drawing.Size(38, 13);
+            this.labelMobile.TabIndex = 3;
+            this.labelMobile.Text = "Mobile";
+            // 
+            // labelType
+            // 
+            this.labelType.AutoSize = true;
+            this.labelType.Location = new System.Drawing.Point(43, 211);
+            this.labelType.Name = "labelType";
+            this.labelType.Size = new System.Drawing.Size(31, 13);
+            this.labelType.TabIndex = 4;
+            this.labelType.Text = "Type";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(103, 70);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(162, 20);
+            this.textBoxName.TabIndex = 5;
+            // 
+            // textBoxAddress
+            // 
+            this.textBoxAddress.Location = new System.Drawing.Point(103, 105);
+            this.textBoxAddress.Name = "textBoxAddress";
+            this.textBoxAddress.Size = new System.Drawing.Size(162, 20);
+            this.textBoxAddress.TabIndex = 6;
+            // 
+            // textBoxAge
+            // 
+            this.textBoxAge.Location = new System.Drawing.Point(103, 135);
+            this.textBoxAge.Name = "textBoxAge";
+            this.textBoxAge.Size = new System.Drawing.Size(162, 20);
+            this.textBoxAge.TabIndex = 7;
+            // 
+            // textBoxMobile
+            // 
+            this.textBoxMobile.Location = new System.Drawing.Point(103, 172);
+            this.textBoxMobile.Name = "textBoxMobile";
+            this.textBoxMobile.Size = new System.Drawing.Size(162, 20);
+            this.textBoxMobile.TabIndex = 8;
+            // 
+            // comboBoxType
+            // 
+            this.comboBoxType.FormattingEnabled = true;
+            this.comboBoxType.Location = new System.Drawing.Point(103, 211);
+            this.comboBoxType.Name = "comboBoxType";
+            this.comboBoxType.Size = new System.Drawing.Size(162, 21);
+            this.comboBoxType.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(103, 247);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(190, 247);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "List";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.Location = new System.Drawing.Point(271, 70);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(400, 173);
+            this.listBox.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(723, 291);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBoxType);
+            this.Controls.Add(this.textBoxMobile);
+            this.Controls.Add(this.textBoxAge);
+            this.Controls.Add(this.textBoxAddress);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.labelType);
+            this.Controls.Add(this.labelMobile);
+            this.Controls.Add(this.labelAge);
+            this.Controls.Add(this.labelAddress);
+            this.Controls.Add(this.labelName);
+            this.Name = "Form1";
+            this.Text = "Contacts";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelAddress;
+        private System.Windows.Forms.Label labelAge;
+        private System.Windows.Forms.Label labelMobile;
+        private System.Windows.Forms.Label labelType;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxAddress;
+        private System.Windows.Forms.TextBox textBoxAge;
+        private System.Windows.Forms.TextBox textBoxMobile;
+        private System.Windows.Forms.ComboBox comboBoxType;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox;
+    }
+}
+
---
+++ MondayWorkshop/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MondayWorkshop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public struct Person
+        {
+            private string name;
+            private string address;
+            private int age;
+            private string telephoneNumber;
+            private string type;
+        
+            public Person(string name, string address, int age, string telephoneNumber, string type)
+            {
+                this.name = name;
+                this.address = address;
+                this.age = age;
+                this.telephoneNumber = telephoneNumber;
+                this.type = type;
+            }
+            public override string ToString()
+            {
+                return string.Format("Name: {0}, Address: {1}, Age: {2}, Mobile: {3}, Type: {4}", name, age, address, telephoneNumber, type.ToString());
+            }
+        }
+        class TelephoneDirectory
+        {
+            public static List<Person> persons = new List<Person>(50);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBoxType.Items.Add("Home");
+            comboBoxType.Items.Add("Workplace");
+            comboBoxType.Items.Add("Mobile");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TelephoneDirectory.persons.Add(new Person(textBoxName.Text, textBoxAddress.Text, Int32.Parse(textBoxAge.Text), textBoxMobile.Text, comboBoxType.Text));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox.Items.Clear();
+            for (int i = 0; i < TelephoneDirectory.persons.Count; i++)
+            {
+                listBox.Items.Add(TelephoneDirectory.persons[i].ToString());
+            }
+        }
+    }
+}
